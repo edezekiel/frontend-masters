8ca3e0f817ca57c743b42fc6a64ecc223c92a81f
Ed Ezekiel <ed@ubiquisoft.com>
2020-06-18 15:10:22 -0500

    Create express server

---
+++ node-example/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.end('Welcome to my site!');
+});
+
+app.get('/complement', function (req, res) {
+  res.end('You look nice today');
+})
+
+app.listen(3000);
+console.log('listening on http://localhost:3000');
+
+// const http = require("http");
+
+// const server = http.createServer(function (req, res) {
+//   console.log(`user visited ${req.url}`);
+//   res.end('hello');
+// });
+
+// server.listen(3000);
