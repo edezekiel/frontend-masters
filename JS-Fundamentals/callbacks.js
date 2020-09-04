f453c831d6969e7fad58ba5bbfb7774dde00edaa
Ed Ezekiel <ed@ubiquisoft.com>
2020-09-04 17:57:00 -0500

    First attempt at the emptyRoom exercise

--- JS-Fundamentals/callbacks.js
+++ JS-Fundamentals/callbacks.js
@@ -1,8 +1,8 @@
 const increment = (n) => n + 1;
 const square = (n) => n * n;
 const doMathSoIDontHaveTo = (n, func) => func(n);
-console.log(doMathSoIDontHaveTo(5, square));
-console.log(doMathSoIDontHaveTo(4, increment));
+// console.log(doMathSoIDontHaveTo(5, square));
+// console.log(doMathSoIDontHaveTo(4, increment));
 
 const _ = {};
 
@@ -28,5 +28,79 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 const myReduceResult = _.reduce(array1, reducer, 10);
 
-console.log("hi hey there");
-console.log(myReduceResult);
+const newDevelopment = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { "dining room": true },
+      { "billiard room": false },
+      { library: true },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": true },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { "dining room": false },
+      { "billiard room": true },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": true },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+
+const emptyRoom = (newDevelopment) => {
+  let emptyRooms = [];
+  const occupiedRooms = [];
+
+  newDevelopment
+    .map((suspect) => suspect.rooms)
+    .flat()
+    .forEach((room) => {
+      const key = Object.keys(room)[0];
+
+      if (
+        !emptyRooms.includes(key) &&
+        !room[key] &&
+        !occupiedRooms.includes(key)
+      ) {
+        emptyRooms.push(key);
+      } else if (room[key]) {
+        occupiedRooms.push(key);
+      }
+    });
+
+  return emptyRooms.filter((room) => !occupiedRooms.includes(room));
+};
+
+emptyRoom(newDevelopment);
